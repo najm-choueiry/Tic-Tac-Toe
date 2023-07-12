eb80fb0d6728700978e20bafd1b06601a64fab4a
najm-choueiry <nachoueiry@outlook.com>
2023-07-12 20:58:52 +0300

    structring the html

---
+++ script/script.js
@@ -0,0 +1,13 @@
+const square1 = document.getElementById("id1")
+const square2 = document.getElementById("id2")
+const square3 = document.getElementById("id3")
+const square4 = document.getElementById("id4")
+const square5 = document.getElementById("id5")
+const square6 = document.getElementById("id6")
+const square7 = document.getElementById("id7")
+const square8 = document.getElementById("id8")
+const square9 = document.getElementById("id9")
+
+
+let canvas = document.querySelector('canvas');
+
