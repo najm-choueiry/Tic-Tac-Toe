fbe966ccf9ba103b09d56f502bf9af6282866633
CHOUEIRY Najm <GSB.NCHOUEIRY@cma-cgm.com>
2023-07-14 13:52:41 +0300

    combinations

--- script/script.js
+++ script/script.js
@@ -1,6 +1,22 @@
 const cellElements = document.querySelectorAll('[data-cell]');
 const board = document.getElementById('board')
 
+
+const winning_combinations = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+
+
+
+
+
 const x_class = 'x'
 const circle_class = 'circle'
 
@@ -22,6 +38,9 @@ function handleClick(e) {
     const cell = e.target
     const currentClass = circleTurn ? circle_class : x_class 
     placeMark (cell, currentClass)   
+    if (checkWin(currentClass)) {
+        console.log('winner')
+    }
     // place mark
     // check for win
     // check for draw
@@ -46,4 +65,13 @@ function setBoardHoverClass() {
     } else {
         board.classList.add(x_class)
     }
+}
+
+
+function checkWin(currentClass) {
+   return winning_combinations.some( combination=> {
+    return combination.every(index => {
+        return cellElements[index].classList.contains(currentClass)
+    })
+   })
 }
