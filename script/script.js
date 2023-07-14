d7f03bd465dde0bf3eb292fb8cc1bd472f1debc5
CHOUEIRY Najm <GSB.NCHOUEIRY@cma-cgm.com>
2023-07-14 13:44:36 +0300

    placing marker - swap turns

--- script/script.js
+++ script/script.js
@@ -1,13 +1,49 @@
-const square1 = document.getElementById("id1")
-const square2 = document.getElementById("id2")
-const square3 = document.getElementById("id3")
-const square4 = document.getElementById("id4")
-const square5 = document.getElementById("id5")
-const square6 = document.getElementById("id6")
-const square7 = document.getElementById("id7")
-const square8 = document.getElementById("id8")
-const square9 = document.getElementById("id9")
+const cellElements = document.querySelectorAll('[data-cell]');
+const board = document.getElementById('board')
+
+const x_class = 'x'
+const circle_class = 'circle'
+
+let circleTurn
 
 
-let canvas = document.querySelector('canvas');
+startGame()
 
+function startGame() {
+    circleTurn = false;
+    cellElements.forEach(cell => {
+        cell.addEventListener('click', handleClick, {once: true})
+    });
+    setBoardHoverClass()
+    
+}
+
+function handleClick(e) {
+    const cell = e.target
+    const currentClass = circleTurn ? circle_class : x_class 
+    placeMark (cell, currentClass)   
+    // place mark
+    // check for win
+    // check for draw
+    // switch turns
+    swapTurns()
+    setBoardHoverClass()
+}
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass)
+}
+
+function swapTurns() {
+    circleTurn  = !circleTurn
+}
+
+function setBoardHoverClass() {
+    board.classList.remove(x_class)
+    board.classList.remove(circle_class_class)
+    if(circleTurn) {
+        board.classList.add(circle_class)
+    } else {
+        board.classList.add(x_class)
+    }
+}
